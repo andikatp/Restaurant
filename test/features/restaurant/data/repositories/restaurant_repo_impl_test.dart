d6c907b8dc9a5da933d1f56edb29a0b45d7506fc
andikatp <triprasetya_andika@yahoo.com>
2023-12-23 11:15:25 +0700

    repo and test

---
+++ test/features/restaurant/data/repositories/restaurant_repo_impl_test.dart
@@ -0,0 +1,57 @@
+import 'package:dartz/dartz.dart';
+import 'package:dicoding_final/core/errors/exception.dart';
+import 'package:dicoding_final/core/errors/failure.dart';
+import 'package:dicoding_final/features/restaurant/data/datasources/restaurant_local_data_source.dart';
+import 'package:dicoding_final/features/restaurant/data/models/restaurant_model.dart';
+import 'package:dicoding_final/features/restaurant/data/repositories/restaurant_repo_impl.dart';
+import 'package:dicoding_final/features/restaurant/domain/repositories/restaurant_repo.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockRestaurantLocalDataSource extends Mock
+    implements RestaurantLocalDataSource {}
+
+void main() {
+  late RestaurantLocalDataSource mockLocalDataSource;
+  late RestaurantRepo repo;
+
+  setUp(() {
+    mockLocalDataSource = MockRestaurantLocalDataSource();
+    repo = RestaurantRepoImpl(dataSource: mockLocalDataSource);
+  });
+
+  final tRestaurants = [RestaurantModel.empty()];
+  const tException = CacheException(message: 'Unknown Failure');
+
+  test('Should call the [RestaurantLocalDataSource.getRestaurant]', () async {
+    // arrange
+    when(() => mockLocalDataSource.getRestaurants())
+        .thenAnswer((_) async => tRestaurants);
+    // act
+    final result = await repo.getRestaurants();
+    // assert
+    expect(result, equals(Right<dynamic, List<RestaurantModel>>(tRestaurants)));
+    verify(() => mockLocalDataSource.getRestaurants()).called(1);
+    verifyNoMoreInteractions(mockLocalDataSource);
+  });
+
+  test(
+      'Should throw a [CacheFailure] when the call to '
+      '[RestaurantLocalDataSource.getRestaurant] unsuccessful', () async {
+    // arrange
+    when(() => mockLocalDataSource.getRestaurants()).thenThrow(tException);
+    // act
+    final result = await repo.getRestaurants();
+    // assert
+    expect(
+      result,
+      equals(
+        Left<Failure, dynamic>(
+          CacheFailure.fromException(tException),
+        ),
+      ),
+    );
+    verify(() => mockLocalDataSource.getRestaurants()).called(1);
+    verifyNoMoreInteractions(mockLocalDataSource);
+  });
+}
