7ee6abad4bf35f0c6b9e061749ebf4f5f0d43849
andikatp <triprasetya_andika@yahoo.com>
2023-12-26 21:29:12 +0700

    detail domain

---
+++ lib/features/detail/domain/usecases/get_detail_restaurant.dart
@@ -0,0 +1,14 @@
+import 'package:dicoding_final/core/usecase/usecase.dart';
+import 'package:dicoding_final/core/utils/typedef.dart';
+import 'package:dicoding_final/features/dashboard/domain/entities/restaurant.dart';
+import 'package:dicoding_final/features/detail/domain/repositories/detail_repo.dart';
+
+class GetDetailRestaurant implements UseCaseWithParams<Restaurant, String> {
+  const GetDetailRestaurant({required DetailRepo repo}) : _repo = repo;
+  final DetailRepo _repo;
+
+  @override
+  ResultFuture<Restaurant> call(String params) {
+    return _repo.getDetailRestaurant();
+  }
+}
