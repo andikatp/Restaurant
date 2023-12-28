ff04954ef64a9276738217668b9c1977b885728b
andikatp <triprasetya_andika@yahoo.com>
2023-12-28 20:28:17 +0700

    next try

--- lib/core/navigation/bottom_navigation.dart
+++ lib/core/navigation/bottom_navigation.dart
@@ -1,47 +1,58 @@
-import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
-import 'package:dicoding_final/features/explore_restaurants/presentation/pages/home_page.dart';
+import 'package:dicoding_final/core/res/colours.dart';
+import 'package:dicoding_final/core/services/injection_container.dart';
+import 'package:dicoding_final/features/explore_restaurants/presentation/cubit/explore_restaurants_cubit.dart';
+import 'package:dicoding_final/features/explore_restaurants/presentation/pages/restaurant_page.dart';
 import 'package:dicoding_final/features/saved_restaurants/presentations/pages/saved_restaurants_page.dart';
 import 'package:dicoding_final/features/settings/presentations/pages/settings_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class BottomNavigation extends StatefulWidget {
+
+
+class BottomNavigation extends StatelessWidget {
   const BottomNavigation({super.key});
+  static const routeName = '/bottom-nav';
 
-  @override
-  State<BottomNavigation> createState() => _BottomNavigationState();
-}
-
-class _BottomNavigationState extends State<BottomNavigation> {
   @override
   Widget build(BuildContext context) {
-    final iconList = <IconData>[Icons.home, Icons.favorite, Icons.settings];
-    final pages = <Widget>[
-      const RestaurantPage(),
-      const SavedRestaurantPage(),
-      const SettingPage(),
-    ];
-    var bottomNavIndex = 0;
-
-    return Scaffold(
-      body: IndexedStack(
-        index: bottomNavIndex,
-        children: pages,
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        activeColor: Colours.primaryColor,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home)),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite)),
+          BottomNavigationBarItem(icon: Icon(Icons.settings)),
+        ],
       ),
-      bottomNavigationBar: AnimatedBottomNavigationBar.builder(
-        itemCount: iconList.length,
-        tabBuilder: (int index, bool isActive) {
-          return Icon(
-            iconList[index],
-            size: 24,
-            color: isActive ? Colors.amber : Colors.blue,
-          );
-        },
-        activeIndex: bottomNavIndex,
-        notchSmoothness: NotchSmoothness.verySmoothEdge,
-        leftCornerRadius: 32,
-        rightCornerRadius: 32,
-        onTap: (index) => setState(() => bottomNavIndex = index),
-      ),
+      tabBuilder: (context, index) {
+        switch (index) {
+          case 0:
+            return CupertinoTabView(
+              builder: (_) => CupertinoPageScaffold(
+                child: BlocProvider<ExploreRestaurantsCubit>(
+                  create: (_) => sl<ExploreRestaurantsCubit>(),
+                  child: const RestaurantPage(),
+                ),
+              ),
+            );
+          case 1:
+            return CupertinoTabView(
+              builder: (_) =>
+                  const CupertinoPageScaffold(child: SavedRestaurantPage()),
+            );
+          case 2:
+            return CupertinoTabView(
+              builder: (_) => const CupertinoPageScaffold(child: SettingPage()),
+            );
+          default:
+            return CupertinoTabView(
+              builder: (_) =>
+                  const CupertinoPageScaffold(child: SavedRestaurantPage()),
+            );
+        }
+      },
+      
     );
   }
 }
