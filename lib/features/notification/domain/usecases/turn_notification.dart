ddb23102c62822ae632a2b0b74a51af1c74f1e5e
andikatp <triprasetya_andika@yahoo.com>
2024-01-01 20:02:54 +0700

    repo and usecase notification

---
+++ lib/features/notification/domain/usecases/turn_notification.dart
@@ -0,0 +1,13 @@
+import 'package:dicoding_final/core/usecase/usecase.dart';
+import 'package:dicoding_final/core/utils/typedef.dart';
+import 'package:dicoding_final/features/notification/domain/repositories/notification_repo.dart';
+
+class TurnNotification implements UseCaseWithoutParams<void, void> {
+  TurnNotification({required NotificationRepo repo}) : _repo = repo;
+  final NotificationRepo _repo;
+
+  @override
+  ResultFuture<void> call() {
+    return _repo.turnNotification();
+  }
+}
