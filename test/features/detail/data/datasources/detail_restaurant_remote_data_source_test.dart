7399e3be6b41fce47525ddf3fff644cf55f3c24f
andikatp <triprasetya_andika@yahoo.com>
2024-01-04 21:50:40 +0700

    test datasource detail

--- test/features/detail/data/datasources/detail_restaurant_remote_data_source_test.dart
+++ test/features/detail/data/datasources/detail_restaurant_remote_data_source_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:dicoding_final/core/constants/api_endpoint.dart';
 import 'package:dicoding_final/core/constants/app_constant.dart';
 import 'package:dicoding_final/core/errors/exception.dart';
@@ -20,46 +22,119 @@ void main() {
     remote = DetailRemoteDataSourceImpl(client: mockClient);
     registerFallbackValue(Uri());
   });
+  tearDown(() {});
 
   const tRestaurant = DetailRestaurantModel.empty();
   const tId = '1';
 
-  test('Should perform a GET request on an URL', () async {
-    // arrange
-    when(() => mockClient.get(any())).thenAnswer(
-      (_) async => http.Response(fixtureReader('detail_restaurant.json'), 200),
-    );
-    // act
-    await remote.getDetailRestaurant(tId);
-    // assert
-    verify(
-      () => mockClient.get(
-        Uri.parse('${AppConstant.baseUrl}${ApiEndpoint.detailRestaurant}/$tId'),
-      ),
-    ).called(1);
-    verifyNoMoreInteractions(mockClient);
+  group('getDetailRestaurant', () {
+    test('Should perform a GET request on an URL', () async {
+      // arrange
+      when(() => mockClient.get(any(that: isA<Uri>()))).thenAnswer(
+        (_) async =>
+            http.Response(fixtureReader('detail_restaurant.json'), 200),
+      );
+      // act
+      await remote.getDetailRestaurant(tId);
+      // assert
+      verify(
+        () => mockClient.get(
+          Uri.parse(
+            '${AppConstant.baseUrl}${ApiEndpoint.detailRestaurant}/$tId',
+          ),
+        ),
+      ).called(1);
+      verifyNoMoreInteractions(mockClient);
+    });
+
+    test('Should return List of [Restaurant] when the response is 200',
+        () async {
+      // arrange
+      when(() => mockClient.get(any(that: isA<Uri>()))).thenAnswer(
+        (_) async =>
+            http.Response(fixtureReader('detail_restaurant.json'), 200),
+      );
+      // act
+      final result = await remote.getDetailRestaurant(tId);
+      // assert
+      expect(result, tRestaurant);
+    });
+
+    test('Should throw a server exception when the response code is not 200',
+        () async {
+      // arrange
+      when(() => mockClient.get(any(that: isA<Uri>()))).thenAnswer(
+        (_) async => http.Response(
+          '{"error": true, "message": "Something went wrong"}',
+          400,
+        ),
+      );
+      // act
+      final result = remote.getDetailRestaurant(tId);
+      // assert
+      expect(result, throwsA(isA<ServerException>()));
+    });
   });
 
-  test('Should return List of [Restaurant] when the response is 200', () async {
-    // arrange
-    when(() => mockClient.get(any())).thenAnswer(
-      (_) async => http.Response(fixtureReader('detail_restaurant.json'), 200),
-    );
-    // act
-    final result = await remote.getDetailRestaurant(tId);
-    // assert
-    expect(result, tRestaurant);
-  });
+  group('reviewRestaurant', () {
+    const tId = 'id';
+    const tReview = 'review';
 
-  test('Should throw a server exception when the response code is not 200',
-      () async {
-    // arrange
-    when(() => mockClient.get(any())).thenAnswer(
-      (_) async => http.Response('Something went wrong', 400),
-    );
-    // act
-    final result = remote.getDetailRestaurant(tId);
-    // assert
-    expect(result, throwsA(isA<ServerException>()));
+    test(
+        'Should perform a POST request on an URL '
+        'when the status code is 201', () async {
+      // arrange
+      when(
+        () => mockClient.post(
+          any(that: isA<Uri>()),
+          body: any(named: 'body'),
+          headers: {'Content-Type': 'application/json'},
+        ),
+      ).thenAnswer(
+        (_) async => http.Response(
+          '{"error": false, "message": "Review has been added successfully"}',
+          201,
+        ),
+      );
+      // act and assert
+      await expectLater(remote.reviewRestaurant(tId, tReview), completes);
+      // assert
+      verify(
+        () => mockClient.post(
+          Uri.parse(
+            '${AppConstant.baseUrl}${ApiEndpoint.postReview}',
+          ),
+          body: jsonEncode(
+            {'id': tId, 'name': 'New User', 'review': tReview},
+          ),
+          headers: {'Content-Type': 'application/json'},
+        ),
+      ).called(1);
+      verifyNoMoreInteractions(mockClient);
+    });
+
+    test('Should throw a server exception when the response code is not 201',
+        () async {
+      // arrange
+      when(
+        () => mockClient.post(
+          any(that: isA<Uri>()),
+          body: any(named: 'body'),
+          headers: {'Content-Type': 'application/json'},
+        ),
+      ).thenAnswer(
+        (_) async => http.Response(
+          '{"error": true, "message": "Something went wrong"}',
+          400,
+        ),
+      );
+      // act
+      final methodCall = remote.reviewRestaurant;
+      // assert
+      expect(
+        methodCall(tId, tReview),
+        throwsA(const ServerException(message: 'Something went wrong')),
+      );
+    });
   });
 }
