485702102cdc04d3c5c0eb0ce2b5f66633ef1ad9
andikatp <triprasetya_andika@yahoo.com>
2024-01-04 18:34:21 +0700

    ey it works, now jst tryna add timer to only 11 am

--- lib/features/settings/data/datasources/notification_data_source.dart
+++ lib/features/settings/data/datasources/notification_data_source.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
 import 'dart:developer';
 import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:dicoding_final/core/errors/exception.dart';
 import 'package:dicoding_final/core/services/notification_service.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:dicoding_final/features/explore_restaurants/data/datasources/remote/explore_restaurants_remote_data_source.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class NotificationDataSource {
   const NotificationDataSource();
@@ -19,57 +21,33 @@ abstract class NotificationDataSource {
 
 class NotificationRemoteDataSourceImpl implements NotificationDataSource {
   NotificationRemoteDataSourceImpl({
-    required FlutterLocalNotificationsPlugin notificationsPlugin,
-  }) : _notificationsPlugin = notificationsPlugin;
-  final FlutterLocalNotificationsPlugin _notificationsPlugin;
+    required ExploreRestaurantsRemoteDataSource explore,
+    required SharedPreferences sharedPreferences,
+  })  : _explore = explore,
+        _sharedPreferences = sharedPreferences;
+
+  final ExploreRestaurantsRemoteDataSource _explore;
+  final SharedPreferences _sharedPreferences;
 
   @override
   Future<void> showNotification({required bool isScheduled}) async {
     if (isScheduled) {
       log('Scheduling News Activated');
-      await AndroidAlarmManager.periodic(
+      final restaurants = await _explore.getRestaurants();
+      final restaurant = restaurants[0];
+      await _sharedPreferences.setString(
+        'currentRestaurant',
+        jsonEncode(restaurant.toJson()),
+      );
+      final result = await AndroidAlarmManager.periodic(
         const Duration(seconds: 5),
         1,
         NotificationService.callback,
       );
+      log(result.toString());
     } else {
       log('Scheduling News Canceled');
       await AndroidAlarmManager.cancel(1);
     }
   }
-
-  Future<void> callback() async {
-    const titleNotification = '<b>Daily Restaurant Near You!</b>';
-    await _notificationsPlugin.show(
-      0,
-      titleNotification,
-      'titleNews',
-      notificationDetails(),
-      payload: 'rqdv5juczeskfw1e867',
-    );
-  }
-
-  NotificationDetails notificationDetails() {
-    const channelId = '1';
-    const channelName = 'channel_01';
-    const channelDescription = 'restaurant channel';
-
-    return const NotificationDetails(
-      android: AndroidNotificationDetails(
-        channelId,
-        channelName,
-        importance: Importance.max,
-        channelDescription: channelDescription,
-        priority: Priority.high,
-        ticker: 'ticker',
-        styleInformation: DefaultStyleInformation(true, true),
-      ),
-      iOS: DarwinNotificationDetails(),
-    );
-  }
 }
-
-
-    // final titleNews = restaurant[0].name;
-
-    // final restaurantList = restaurant.map((e) => e.toJson()).toList();
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,11 +8,13 @@ import Foundation
 import connectivity_plus
 import flutter_local_notifications
 import path_provider_foundation
+import shared_preferences_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
